1ecc80f30202ca12d9f3f64824864567a4e85e26
Vic-L <vljc17@gmail.com>
2019-03-05 15:54:14 -1000

    feat: list md pages in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,60 @@
 import React from "react"
-import { Link } from "gatsby"
+import {
+  Link,
+  graphql,
+} from "gatsby"
+import styled from 'styled-components';
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const PageWrapper = styled.div`
+  border: 1px solid #e0e0e0;
+  border-radius: 4px;
+`;
+
+const DateSpan = styled.span`
+  color: #bbb;
+`;
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+      {
+        data.allMarkdownRemark.edges.map(({ node }) => (
+          <PageWrapper>
+            <h2>{node.frontmatter.title}{" "}</h2>
+            <DateSpan>— {node.frontmatter.date}</DateSpan>
+            <p>{node.excerpt}</p>
+          </PageWrapper>
+        ))
+      }
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
